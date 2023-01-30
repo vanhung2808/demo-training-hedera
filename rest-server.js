19f49ff1117237397f6326f77b77981fe9013b75
DuyHVN <hvnduy1504@gmail.com>
2023-01-30 18:36:23 +0700

    Config SwaggerUI

--- rest-server.js
+++ rest-server.js
@@ -1,11 +1,19 @@
-const express = require('express'); //Line 1
-const app = express(); //Line 2
-const port = process.env.PORT || 5000; //Line 3
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+);
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
 // create a GET route
-app.get('/api/v1/hedera', (req, res) => { //Line 9
+app.get('/api/v1/hedera', (req, res) => {
     res.send({ express: 'Welcome to Hedera — let’s build the future' });
-}); //Line 11
+});
